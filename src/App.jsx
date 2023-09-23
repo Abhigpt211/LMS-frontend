e699daa4fc38549f8ae8044b3daec02350d049ec
Abhigpt211 <abhigpt211@gmail.com>
2023-09-23 14:03:00 +0530

    Added some files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,13 @@ import HomePage from './Pages/HomePage'
 import AboutUs from './Pages/AboutUs'
 import NotFound from './Pages/NotFound'
 import Signup from './Pages/Signup'
+import Login from './Pages/Login'
+import CourseList from './Pages/Course/CourseList'
+import Contact from './Pages/Contact'
+import Denied from './Pages/Denied'
+import CourseDescription from './Pages/Course/CourseDescription'
+import RequireAuth from './Components/Auth/RequireAuth'
+import CreateCourse from './Pages/Course/CreateCourse'
 
 
 function App() {
@@ -12,8 +19,18 @@ function App() {
     <Routes> 
       <Route path='/' element={<HomePage/>}></Route>
       <Route path='/about' element={<AboutUs/>}></Route>
+      <Route path='/courses' element={<CourseList/>}></Route>
+      <Route path='/contact' element={<Contact/>}></Route>
+      <Route path='/denied' element={<Denied/>}></Route>
+
+      <Route path='/course/description' element={<CourseDescription/>}></Route>
        
-       <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      
+      <Route element={<RequireAuth allowedRoles={["ADMIN"]}/> }>
+      <Route path='/course/create' element={<CreateCourse/>}></Route>
+      </Route>
 
       <Route path='*' element={<NotFound/>}></Route>
     </Routes>  
--- src/Helpers/axiosInstance.js
+++ src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:5025/api/v1";
+const BASE_URL = "http://localhost:5026/api/v1";
 
 const axiosInstance = axios.create();
 
